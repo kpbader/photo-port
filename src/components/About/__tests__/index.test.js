8951125723827571120437d96eb0f0079471eba5
Kyle Patrick Bader <kbader91@gmail.com>
2022-05-02 06:42:38 -0700

    testing implemented and passing

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,23 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
-// will ensure after each test there will not be any leftover memory data (that could lead to false results) 
 afterEach(cleanup);
 
 describe('About component', () => {
-    // First test 
-    it('renders', () => {
-        render(<About />);
-    });
-
-    // Second test 
-    it('matches snapshot DOM node structure', () => {
-        // render About 
-        const { asFragment } = render(<About />);
-
-        expect(asFragment()).toMatchSnapshot();
-    })
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
-
--- src/components/Nav/index 2.js
+++ src/components/Nav/index 2.js
@@ -2,7 +2,6 @@ import React from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav() {
-
   const  categories = [
     { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
     { name: 'portraits', description: 'Portraits of people in my life' },
@@ -10,34 +9,33 @@ function Nav() {
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' }
   ];
 
-  const handleClick = (item) => {
-    console.log(item);
-    return item;
-  };
+  const handleClick = () => {
+    console.log("click handled")
+  }
 
   return (
-    <header className="flex-row px-1">
+    <header data-testid="header" className="flex-row px-1">
       <h2>
-        <a data-testid="link" href="/">
-          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+      <a data-testid="link" href="/">
+  <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about" onClick={() => handleClick("About")}>
+            <a data-testid="about" href="#about">
               About me
             </a>
           </li>
           <li className={"mx-2"}>
-            <span onClick={() => handleClick('Contact')}>
+            <span onClick={() => handleClick()}>
               Contact
             </span>
           </li>
           {
             categories.map((category) => (
               <li className="mx-1" key={category.name} >
-                <span onClick={() => { handleClick(category.name); }}>
+                <span onClick={() => { handleClick(); }}>
                  {capitalizeFirstLetter(category.name)}
                 </span>
               </li>
