28ae9093e69a18befdbec70a088316f13586f1c6
Kyle Patrick Bader <kbader91@gmail.com>
2022-05-03 19:57:35 -0700

    photo gallery updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-import Gallery from './components/Gallery'
+import Gallery from './components/Gallery';
 
 function App() {
   const [categories] = useState([
@@ -14,18 +14,18 @@ function App() {
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
 
-  const [currentCategory, setCurretCategory] = useState(categories[0]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
     <div>
       <Nav
         categories={categories}
-        setCurretCategory={setCurretCategory}
+        setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-        ></Nav>
+      ></Nav>
       <main>
-          <Gallery></Gallery>
-          <About></About>      
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
